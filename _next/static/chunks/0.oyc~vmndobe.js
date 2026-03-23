f018872b3125cda12dcc0789ece295625b1d9ddb
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:11:49 -0300

    deploy: _next/static/chunks/0.oyc~vmndobe.js

---
+++ _next/static/chunks/0.oyc~vmndobe.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/campanhas", [
+  "static/chunks/16v91-cl60w7n.js",
+  "static/chunks/02nq.3g9kv79y.js",
+  "static/chunks/0w.-28hkkr_2h.js",
+  "static/chunks/0i.6p5~hf1ln1.js",
+  "static/chunks/turbopack-135e.26c39~tm.js"
+])
