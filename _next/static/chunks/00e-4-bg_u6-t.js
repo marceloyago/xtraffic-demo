a506d1a49c82cfec4dbee0547c2b307bb7304188
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:27:10 -0300

    update: _next/static/chunks/00e-4-bg_u6-t.js

---
+++ _next/static/chunks/00e-4-bg_u6-t.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/0l-jthsx_y-iz.js",
+  "static/chunks/0~p2o9vrap6j_.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-12-uyhmnl~142.js"
+])
