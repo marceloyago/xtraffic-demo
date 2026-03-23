4c9e2296fa9a0cf73538d6d92a5c0d7c0bd2f975
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:29:58 -0300

    update: _next/static/chunks/0a.deg~5_cbyv.js

---
+++ _next/static/chunks/0a.deg~5_cbyv.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/geracao", [
+  "static/chunks/00vgro24ki7y8.js",
+  "static/chunks/0r~uj0~8wtz45.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0kbkzpwm1llmn.js"
+])
