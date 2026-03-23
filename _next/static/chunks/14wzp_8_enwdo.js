df9e3d5fb5efa35dcdc596df756f94145bd90432
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:12:10 -0300

    deploy: _next/static/chunks/14wzp_8_enwdo.js

---
+++ _next/static/chunks/14wzp_8_enwdo.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/plano", [
+  "static/chunks/0rc7tuors-_kr.js",
+  "static/chunks/02nq.3g9kv79y.js",
+  "static/chunks/0w.-28hkkr_2h.js",
+  "static/chunks/0i.6p5~hf1ln1.js",
+  "static/chunks/turbopack-0th4za00ww2_x.js"
+])
