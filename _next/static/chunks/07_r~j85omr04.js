f6e111be08c3172496a55a2ed682329935524ffe
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:27:05 -0300

    update: _next/static/chunks/07_r~j85omr04.js

---
+++ _next/static/chunks/07_r~j85omr04.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/plano", [
+  "static/chunks/0rc7tuors-_kr.js",
+  "static/chunks/0uhh3bel_zaop.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0wqp481.h2ooj.js"
+])
