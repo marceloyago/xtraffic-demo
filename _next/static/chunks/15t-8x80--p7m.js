c3df2d66fde3b6c363d629b9413b7d4c27f0aa45
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:12:11 -0300

    deploy: _next/static/chunks/15t-8x80--p7m.js

---
+++ _next/static/chunks/15t-8x80--p7m.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/geracao", [
+  "static/chunks/00vgro24ki7y8.js",
+  "static/chunks/02nq.3g9kv79y.js",
+  "static/chunks/0w.-28hkkr_2h.js",
+  "static/chunks/0i.6p5~hf1ln1.js",
+  "static/chunks/turbopack-0aqcifhap3~v0.js"
+])
