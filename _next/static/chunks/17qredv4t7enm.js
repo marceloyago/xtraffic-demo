b5349866372f7f40c2200301b40128bfcf883451
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:27:15 -0300

    update: _next/static/chunks/17qredv4t7enm.js

---
+++ _next/static/chunks/17qredv4t7enm.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/campanhas", [
+  "static/chunks/16v91-cl60w7n.js",
+  "static/chunks/0uhh3bel_zaop.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-16--zf16x-2su.js"
+])
