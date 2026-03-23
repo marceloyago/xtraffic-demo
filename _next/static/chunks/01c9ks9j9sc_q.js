b55d69d6f6a801080a9b3fa022debc6f5e02760d
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:30:16 -0300

    update: _next/static/chunks/01c9ks9j9sc_q.js

---
+++ _next/static/chunks/01c9ks9j9sc_q.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/decisoes-ia", [
+  "static/chunks/0kz1ejenybv.v.js",
+  "static/chunks/0842bya.wqb5y.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-03.2pn9-ymqmz.js"
+])
