b4de9931c891c02518755a7bc81ecb5c188ee35d
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:14:26 -0300

    deploy: _next/static/chunks/15wzwnyv3dwwz.js

---
+++ _next/static/chunks/15wzwnyv3dwwz.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/decisoes-ia", [
+  "static/chunks/0kz1ejenybv.v.js",
+  "static/chunks/0r-1dmlcf0vmh.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-156dx4331xhan.js"
+])
