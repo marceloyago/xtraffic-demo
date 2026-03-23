16acd6dbaf39a623aa608aa934ab5d691fd5876d
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:30:12 -0300

    update: _next/static/chunks/0hgt-2wb2p9sv.js

---
+++ _next/static/chunks/0hgt-2wb2p9sv.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/0l-jthsx_y-iz.js",
+  "static/chunks/15ge-ld6x89w6.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0ha1_l1h963hc.js"
+])
