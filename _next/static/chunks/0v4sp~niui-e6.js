84cea584647c056cf3b96c56ae72facfcaf5cc56
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:26:58 -0300

    update: _next/static/chunks/0v4sp~niui-e6.js

---
+++ _next/static/chunks/0v4sp~niui-e6.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/geracao", [
+  "static/chunks/00vgro24ki7y8.js",
+  "static/chunks/0uhh3bel_zaop.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0~.8auoem0xx-.js"
+])
