cae3b0dace3584ef48565c215e21cbbbac30ab3b
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:27:03 -0300

    update: _next/static/chunks/0n3dbm~wt83ku.js

---
+++ _next/static/chunks/0n3dbm~wt83ku.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/configuracoes", [
+  "static/chunks/08hdbmckz4y7d.js",
+  "static/chunks/0uhh3bel_zaop.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-04y5~y7zp.at9.js"
+])
