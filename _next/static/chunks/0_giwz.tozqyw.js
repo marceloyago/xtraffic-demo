d94ce0dd24b6c772a6fa607b7fcacd9608b4c89a
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:11:56 -0300

    deploy: _next/static/chunks/0_giwz.tozqyw.js

---
+++ _next/static/chunks/0_giwz.tozqyw.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/configuracoes", [
+  "static/chunks/08hdbmckz4y7d.js",
+  "static/chunks/02nq.3g9kv79y.js",
+  "static/chunks/0w.-28hkkr_2h.js",
+  "static/chunks/0i.6p5~hf1ln1.js",
+  "static/chunks/turbopack-0pj6smcrxsioq.js"
+])
