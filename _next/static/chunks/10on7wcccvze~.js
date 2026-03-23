c3a64d16fb6d10c2027e5030e0673752bb427014
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:30:01 -0300

    update: _next/static/chunks/10on7wcccvze~.js

---
+++ _next/static/chunks/10on7wcccvze~.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/login", [
+  "static/chunks/0b.tml3sgj281.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/turbopack-0gicoi8bh~1ux.js"
+])
