71e3939dbcaeb0bd7f31e2ca941d698d10d48607
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:14:17 -0300

    deploy: _next/static/chunks/0-ziq8lzlnbu3.js

---
+++ _next/static/chunks/0-ziq8lzlnbu3.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/login", [
+  "static/chunks/0_k82_qnn5-8i.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/turbopack-0kbtdaejpn_de.js"
+])
