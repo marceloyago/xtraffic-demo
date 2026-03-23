e2301557a38b6c2a6d20e55b1e4858e7a9615558
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:12:09 -0300

    deploy: _next/static/chunks/14-vp_i-cs6.z.js

---
+++ _next/static/chunks/14-vp_i-cs6.z.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/03m-f9xqbev39.js",
+  "static/chunks/0i.6p5~hf1ln1.js",
+  "static/chunks/0w.-28hkkr_2h.js",
+  "static/chunks/11vo_tv0_.l.d.css",
+  "static/chunks/turbopack-0fuo46hrls2pt.js"
+])
