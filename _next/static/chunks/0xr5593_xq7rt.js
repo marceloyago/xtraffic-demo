d7dbf9ed058a77bae79e0b90b593bd7de0de17ed
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:30:04 -0300

    update: _next/static/chunks/0xr5593_xq7rt.js

---
+++ _next/static/chunks/0xr5593_xq7rt.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/contas", [
+  "static/chunks/09v9ep3b_l-f5.js",
+  "static/chunks/0r~uj0~8wtz45.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0-p-f1k8wtbsk.js"
+])
