aa2faf9f331c8c0a083bfaaf0ea02fb94ea644a8
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:14:13 -0300

    deploy: _next/static/chunks/0q5ik8vngvyvz.js

---
+++ _next/static/chunks/0q5ik8vngvyvz.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/contas", [
+  "static/chunks/09v9ep3b_l-f5.js",
+  "static/chunks/02nq.3g9kv79y.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0nrxaln31yinn.js"
+])
