478ac1590ef0e94749e7daa5579b6b25cc5fcbb5
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:27:21 -0300

    update: _next/static/chunks/15.dw.-76vg~e.js

---
+++ _next/static/chunks/15.dw.-76vg~e.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/relatorios", [
+  "static/chunks/0huc7wuhym-l_.js",
+  "static/chunks/0uhh3bel_zaop.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0gczrl78vd6aj.js"
+])
