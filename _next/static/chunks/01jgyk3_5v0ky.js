101ec9aaad5fb54ff14cc88c3b9349e56ecc4a51
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:27:16 -0300

    update: _next/static/chunks/01jgyk3_5v0ky.js

---
+++ _next/static/chunks/01jgyk3_5v0ky.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/suporte", [
+  "static/chunks/0inc7mqbmr8-4.js",
+  "static/chunks/0uhh3bel_zaop.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-09y9dk2xw1wm2.js"
+])
