5bf206448f097507c7056d4f4a8115d078633274
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-23 18:30:19 -0300

    update: _next/static/chunks/0zy9h61fg.elb.js

---
+++ _next/static/chunks/0zy9h61fg.elb.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/plano", [
+  "static/chunks/0rc7tuors-_kr.js",
+  "static/chunks/0r~uj0~8wtz45.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0l5i6lck366i8.js"
+])
