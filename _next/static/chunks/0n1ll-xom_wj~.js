340f3c89f87734dd5a0cc5cb358205ae251ecd18
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-27 16:20:10 -0300

    deploy: _next/static/chunks/0n1ll-xom_wj~.js

---
+++ _next/static/chunks/0n1ll-xom_wj~.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/plano", [
+  "static/chunks/0rc7tuors-_kr.js",
+  "static/chunks/14hi369my81id.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-07zvf-9w22tg2.js"
+])
