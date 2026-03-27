dadbb51399f9871b3aee7b795b2667869d1ef2fb
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-27 16:20:18 -0300

    deploy: _next/static/chunks/00f-7-nb~99uo.js

---
+++ _next/static/chunks/00f-7-nb~99uo.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/geracao", [
+  "static/chunks/00vgro24ki7y8.js",
+  "static/chunks/14hi369my81id.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0b70l4~mibwcu.js"
+])
