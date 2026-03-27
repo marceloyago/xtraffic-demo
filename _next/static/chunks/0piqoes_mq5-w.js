13d34d2c9d3f89197cffb8cdd90a3a56998a142b
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-27 16:20:07 -0300

    deploy: _next/static/chunks/0piqoes_mq5-w.js

---
+++ _next/static/chunks/0piqoes_mq5-w.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/decisoes-ia", [
+  "static/chunks/0kz1ejenybv.v.js",
+  "static/chunks/039e7_y8pjnt3.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-0jm08y1sdzwm8.js"
+])
