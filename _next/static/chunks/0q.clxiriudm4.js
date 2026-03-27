6c3fe6bb45d5fedeb959baeeb38a3b09a4b25c15
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-27 16:20:02 -0300

    deploy: _next/static/chunks/0q.clxiriudm4.js

---
+++ _next/static/chunks/0q.clxiriudm4.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/0l-jthsx_y-iz.js",
+  "static/chunks/0s_t3gt.ipf-g.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-10i-9j4h_i18j.js"
+])
