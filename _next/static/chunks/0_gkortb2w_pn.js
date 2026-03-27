849de191b7135b45ff82e7dc6e90002943376994
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-27 16:20:25 -0300

    deploy: _next/static/chunks/0_gkortb2w_pn.js

---
+++ _next/static/chunks/0_gkortb2w_pn.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/contas", [
+  "static/chunks/09v9ep3b_l-f5.js",
+  "static/chunks/14hi369my81id.js",
+  "static/chunks/0mkduktuefubq.js",
+  "static/chunks/0ibcu1z1paxkz.js",
+  "static/chunks/turbopack-17_-~31mahxc8.js"
+])
