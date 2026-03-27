6b3a3d9bf30da447e48276060510604855335a45
marceloyago <85968994+marceloyago@users.noreply.github.com>
2026-03-27 16:20:00 -0300

    deploy: _next/static/3_GuRpSZY9MtZWOeW_2a2/_buildManifest.js

---
+++ _next/static/3_GuRpSZY9MtZWOeW_2a2/_buildManifest.js
@@ -0,0 +1,55 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0q.clxiriudm4.js"
+  ],
+  "/_error": [
+    "static/chunks/0bs68p2pzd161.js"
+  ],
+  "/campanhas": [
+    "static/chunks/0-lmditbeq14i.js"
+  ],
+  "/configuracoes": [
+    "static/chunks/0xb33pes76ju8.js"
+  ],
+  "/contas": [
+    "static/chunks/0_gkortb2w_pn.js"
+  ],
+  "/decisoes-ia": [
+    "static/chunks/0piqoes_mq5-w.js"
+  ],
+  "/geracao": [
+    "static/chunks/00f-7-nb~99uo.js"
+  ],
+  "/login": [
+    "static/chunks/10on7wcccvze~.js"
+  ],
+  "/plano": [
+    "static/chunks/0n1ll-xom_wj~.js"
+  ],
+  "/relatorios": [
+    "static/chunks/03oq0l316ced0.js"
+  ],
+  "/suporte": [
+    "static/chunks/09210g6hbf~bo.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/hello",
+    "/campanhas",
+    "/configuracoes",
+    "/contas",
+    "/decisoes-ia",
+    "/geracao",
+    "/login",
+    "/plano",
+    "/relatorios",
+    "/suporte"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
